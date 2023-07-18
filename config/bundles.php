208544503652e40b1574f48a792dcf9ea75cd7f2
MercuryKojo <kojo.13@gmail.com>
2023-07-18 19:13:33 +0200

    add db dump, update htaccess and update readme

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,7 @@ return [
     Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
     Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
     Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
-    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
     Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
 ];
