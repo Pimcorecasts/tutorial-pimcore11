49aebead9ff46f0d36372592887a06d1f9112a1c
MercuryKojo <kojo.13@gmail.com>
2023-07-18 18:50:52 +0200

    setup pimcore using docker

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+];
